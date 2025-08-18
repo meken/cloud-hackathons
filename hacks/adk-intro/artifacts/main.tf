bc8600e623f45899ad1d2ece8d763ab1928a4734
Murat Eken <meken@google.com>
2025-08-18 17:10:49 +0200

    added a new challenge and started on the TF...

--- hacks/adk-intro/artifacts/main.tf
+++ hacks/adk-intro/artifacts/main.tf
@@ -11,7 +11,124 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "google_project_service" "compute_api" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false  
+resource "google_project_service" "default" {
+  project = var.gcp_project_id
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com",
+    "compute.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "run.googleapis.com",
+    "logging.googleapis.com"
+  ])
+  service = each.key
+
+  disable_on_destroy = false
 }
+
+data "google_compute_default_service_account" "gce_default" {
+  depends_on = [
+    google_project_service.default
+  ]
+}
+
+# In case a default network is not present in the project the variable `create_default_network` needs to be set.
+resource "google_compute_network" "default_network_created" {
+  name                    = "default"
+  auto_create_subnetworks = true
+  count                   = var.create_default_network ? 1 : 0
+  depends_on = [
+    google_project_service.default
+  ]
+}
+
+resource "google_compute_firewall" "fwr_allow_custom" {
+  name          = "fwr-ingress-allow-custom"
+  network       = google_compute_network.default_network_created[0].self_link
+  count         = var.create_default_network ? 1 : 0
+  source_ranges = ["10.128.0.0/9"]
+  allow {
+    protocol = "all"
+  }
+}
+
+resource "google_compute_firewall" "fwr_allow_iap" {
+  name          = "fwr-ingress-allow-iap"
+  network       = google_compute_network.default_network_created[0].self_link
+  count         = var.create_default_network ? 1 : 0
+  source_ranges = ["35.235.240.0/20"]
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+# This piece of code makes it possible to deal with the default network the same way, regardless of how it has
+# been created. Make sure to refer to the default network through this resource when needed.
+data "google_compute_network" "default_network" {
+  name = "default"
+  depends_on = [
+    google_project_service.default,
+    google_compute_network.default_network_created
+  ]
+}
+
+resource "google_sourcerepo_repository" "repo" {
+  name       = "ghacks-adk-intro"
+  depends_on = [google_project_service.default]
+}
+
+resource "google_service_account" "startup_vm_sa" {
+  account_id   = "sa-bqdwh-startup-vm"
+  display_name = "Startup VM Service Account"
+}
+
+resource "google_project_iam_member" "startup_vm_sa_roles" {
+  project = var.gcp_project_id
+  for_each = toset([
+    "roles/storage.objectAdmin"
+  ])
+  role   = each.key
+  member = "serviceAccount:${google_service_account.startup_vm_sa.email}"
+}
+
+resource "google_compute_instance" "startup_vm" {
+  name         = "gce-lnx-env-setup"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  shielded_instance_config {
+    enable_secure_boot = true
+    enable_vtpm        = true
+  }
+
+  network_interface {
+    network = data.google_compute_network.default_network.self_link
+    access_config {}
+  }
+
+  service_account {
+    email  = google_service_account.startup_vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+
+  metadata_startup_script = templatefile("${path.module}/setup.tftpl", {
+    gcp_project_id = var.gcp_project_id,
+    gcp_region     = var.gcp_region,
+    source_repo    = google_sourcerepo_repository.repo.name
+  })
+}
+
+
+
+
