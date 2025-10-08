c5c8508cbfc9a58b4f580777bfa25ee5cc9ce307
Gino Filicetti <ginof@google.com>
2025-10-08 13:37:27 -0400

    added more apis and bucket creation

--- hacks/genmedia-on-gcp/artifacts/main.tf
+++ hacks/genmedia-on-gcp/artifacts/main.tf
@@ -11,7 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+resource "google_project_service" "serviceusage_api" {
+  service            = "serviceusage.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "resource_manager_api" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "compute_api" {
   service            = "compute.googleapis.com"
-  disable_on_destroy = false  
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "vertex_api" {
+  service            = "aiplatform.googleapis.com"
+  disable_on_destroy = false
+}
+
+data "google_storage_project_service_account" "gcs_default" {
+}
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "${var.gcp_project_id}-creative-assets"
+  location                    = var.gcp_region
+  uniform_bucket_level_access = true
 }
