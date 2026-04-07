3f15e92978c8823186cd8ee74ee44adf309267be
Peter Bavinck <pbavinck@google.com>
2026-04-07 13:16:21 +0000

    removed projectIamAdmin from compute SA

--- hacks/bq-olh/artifacts/main.tf
+++ hacks/bq-olh/artifacts/main.tf
@@ -47,7 +47,7 @@ data "google_project" "project" {}
 
 resource "google_project_iam_member" "compute_sa_roles" {
   for_each = toset([
-    "roles/resourcemanager.projectIamAdmin",
+    # "roles/resourcemanager.projectIamAdmin",
     "roles/dataproc.worker",
     "roles/dataproc.editor",
     "roles/bigquery.connectionAdmin",
